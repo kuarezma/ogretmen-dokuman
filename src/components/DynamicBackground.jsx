a2a873449949008810d17241e60b7710a376f469
ugur <yasayanugur@gmail.com>
2026-03-22 20:57:15 +0300

    style: remove floating orbs from background

--- src/components/DynamicBackground.jsx
+++ src/components/DynamicBackground.jsx
@@ -4,9 +4,6 @@ import './DynamicBackground.css';
 const DynamicBackground = () => {
   return (
     <div className="dynamic-bg-container">
-      <div className="bg-shape bg-shape-1"></div>
-      <div className="bg-shape bg-shape-2"></div>
-      <div className="bg-shape bg-shape-3"></div>
       <div className="bg-grid"></div>
       
       {/* Floating educational icons or shapes (subtle dots for a minimal feel) */}
